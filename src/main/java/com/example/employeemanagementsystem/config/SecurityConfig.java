13070733ddde59a4db310ace5221fcb8357c6ab5
Swapnil Gaonkar <gaonkarswapnil1@gmail.com>
2025-09-29 09:27:58 +0530

    for custom user(Static) not from database

--- src/main/java/com/example/employeemanagementsystem/config/SecurityConfig.java
+++ src/main/java/com/example/employeemanagementsystem/config/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,4 +28,22 @@ public class SecurityConfig {
         return http.build();
     }
 
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user1 = User.withDefaultPasswordEncoder()
+                .username("oliver")
+                .password("abc@123")
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.withDefaultPasswordEncoder()
+                .username("swapnil")
+                .password("abc@123")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
 }
